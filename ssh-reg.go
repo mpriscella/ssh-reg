6d8e53cbebcc82b948f535d260bd7f3e2232050f
Mike Priscella <mpriscella@gmail.com>
2015-07-29 14:08:21 -0400

    Updated commands

--- ssh-reg.go
+++ ssh-reg.go
@@ -11,49 +11,78 @@ import (
 )
 
 var (
-	host         = kingpin.Arg("host", "The name of the host").String()
-	hostName     = kingpin.Arg("hostname", "The HostName of the specified host").String()
-	remove       = kingpin.Flag("remove", "Sest to remove the specified host").Bool()
-	identityFile = kingpin.Flag("identity", "The location of the hosts private key").Default("").Short('i').String()
-	user         = kingpin.Flag("user", "The SSH User").Default("").Short('u').String()
-	force        = kingpin.Flag("force", "Overwrite the specified host").Bool()
-	update       = kingpin.Flag("update", "Update the specified host").Bool()
-	list         = kingpin.Flag("list", "List the available hosts").Short('l').Bool()
+	app = kingpin.New("ssh-reg", "A ssh config management tool.")
+
+	list = app.Command("list", "List all available hosts")
+
+	add             = app.Command("add", "Add host")
+	addHost         = add.Arg("host", "The name of the host").Required().String()
+	addHostName     = add.Arg("hostname", "The HostName of the specified host").Required().String()
+	addIdentityFile = add.Flag("identity", "The location of the hosts private key").Default("").Short('i').String()
+	addUser         = add.Flag("user", "The SSH User").Default("").Short('u').String()
+	addForce        = add.Flag("force", "Overwrite the specified host").Bool()
+
+	remove     = app.Command("remove", "Remove host")
+	removeHost = remove.Arg("host", "The name of the host").String()
+
+	update             = app.Command("update", "Update host")
+	updateHost         = update.Arg("host", "The name of the host").String()
+	updateHostName     = update.Arg("hostname", "The HostName of the specified host").String()
+	updateIdentityFile = update.Flag("identity", "The location of the hosts private key").Default("").Short('i').String()
+	updateUser         = update.Flag("user", "The SSH User").Default("").Short('u').String()
 )
 
 var ssh_config string
 
 func main() {
-	kingpin.Version("0.0.2")
-	kingpin.Parse()
-
+	kingpin.Version("0.0.3")
 	usr, _ := User.Current()
 	dir := usr.HomeDir
 	ssh_config = dir + "/.ssh/config"
 	fh, _ := os.OpenFile(ssh_config, os.O_RDWR|os.O_APPEND, 0777)
 
-	hostExists := searchHost(*host)
-	if *list {
-		listHosts()
-	} else if hostExists {
-		if *force {
-			removeHost(*host)
-			addHost(*host, *hostName, *identityFile, *user)
-		} else if *update {
-			updateHost(*host, *hostName, *identityFile, *user)
-		} else if *remove {
-			removeHost(*host)
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case list.FullCommand():
+		_listHosts()
+		break
+	case add.FullCommand():
+		hostExists := _searchHost(*addHost)
+		if hostExists {
+			if *addForce {
+				_removeHost(*addHost)
+				_addHost(*addHost, *addHostName, *addIdentityFile, *addUser)
+			} else {
+				fmt.Println(fmt.Sprintf("Host '%v' already exists. Use --force to overwrite.", *addHost))
+				app.Usage(os.Args[1:])
+			}
 		} else {
-			fmt.Println("Host exists, use --force to overwrite.")
-			kingpin.Usage()
+			_addHost(*addHost, *addHostName, *addIdentityFile, *addUser)
 		}
-	} else {
-		addHost(*host, *hostName, *identityFile, *user)
+		break
+	case remove.FullCommand():
+		hostExists := _searchHost(*removeHost)
+		if hostExists {
+			_removeHost(*removeHost)
+		} else {
+			fmt.Println(fmt.Printf("Host '%v' doesn't exist.", *removeHost))
+			app.Usage(os.Args[1:])
+		}
+		break
+	case update.FullCommand():
+		hostExists := _searchHost(*updateHost)
+		if hostExists {
+			_updateHost(*updateHost, *updateHostName, *updateIdentityFile, *updateUser)
+		} else {
+			fmt.Println(fmt.Printf("Host '%v' doesn't exist.", *updateHost))
+			app.Usage(os.Args[1:])
+		}
+		break
 	}
+
 	defer fh.Close()
 }
 
-func searchHost(host string) bool {
+func _searchHost(host string) bool {
 	regex, _ := regexp.Compile(fmt.Sprintf("^Host %v$", host))
 
 	input, _ := ioutil.ReadFile(ssh_config)
@@ -67,7 +96,7 @@ func searchHost(host string) bool {
 	return false
 }
 
-func listHosts() {
+func _listHosts() {
 	regex, _ := regexp.Compile(`^Host (.+)$`)
 
 	input, _ := ioutil.ReadFile(ssh_config)
@@ -81,11 +110,7 @@ func listHosts() {
 	}
 }
 
-func addHost(host string, hostName string, identityFile string, user string) {
-	if hostName == "" {
-		kingpin.Usage()
-		return
-	}
+func _addHost(host string, hostName string, identityFile string, user string) {
 	fh, _ := os.OpenFile(ssh_config, os.O_RDWR|os.O_APPEND, 0777)
 	fh.WriteString(fmt.Sprintf("Host %v\n", host))
 	fh.WriteString(fmt.Sprintf("  HostName %v\n", hostName))
@@ -99,11 +124,11 @@ func addHost(host string, hostName string, identityFile string, user string) {
 	defer fh.Close()
 }
 
-func updateHost(host string, hostName string, identityFile string, user string) {
-
+func _updateHost(host string, hostName string, identityFile string, user string) {
+	fmt.Printf("Not implemented yet.")
 }
 
-func removeHost(host string) {
+func _removeHost(host string) {
 	hostRegex, _ := regexp.Compile(fmt.Sprintf("^Host %v$", host))
 	regex, _ := regexp.Compile("^Host .+$")
 	input, _ := ioutil.ReadFile(ssh_config)
